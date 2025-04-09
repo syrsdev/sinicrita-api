1ba518f3b51528eb1ba1c0e5d580586d1c2dcc5a
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-09 13:29:30 +0700

    feat: login and register api

---
+++ app/Http/Controllers/v1/Auth/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+
+            $credentials = $request->validate([
+                'username' => ['required', 'unique:users,username'],
+                'password' => ['required'],
+                'role' => ['required'],
+            ]);
+
+            User::create($credentials);
+
+            return response()->json(['status' => 'success', 'statusCode' => '200', 'message' => 'User registration successful'], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 'error', 'statusCode' => '500', 'message' => $th->getMessage()], 500);
+        }
+    }
+    public function login(Request $request)
+    {
+        try {
+            $credentials = $request->validate([
+                'username' => ['required'],
+                'password' => ['required'],
+            ]);
+
+            $user = User::where('username', $credentials['username'])->first();
+
+            if (!$user) {
+                return response()->json(['status' => 'error', 'statusCode' => '404', 'message' => 'User not found'], 404);
+            }
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json(['status' => 'error', 'statusCode' => '401', 'message' => 'Unauthorized'], 401);
+            }
+
+            $request->session()->regenerate();
+            return response()->json(['status' => 'success', 'statusCode' => '200', 'message' => 'Login successful'], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 'error', 'statusCode' => '500', 'message' => $th->getMessage()], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\v1\Auth\AuthController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/v1/register', AuthController::class . '@register');
+Route::post('/v1/login', AuthController::class . '@login');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
