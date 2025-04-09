e1e42956a29b3203f4292f046d58459c8059fbfd
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-09 13:28:59 +0700

    fix: model error

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             // $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'pendengar', 'pencerita']);
+            $table->enum('role', ['admin', 'pendengar', 'pencerita'])->default('pendengar');;
             $table->rememberToken();
             $table->timestamps();
         });
