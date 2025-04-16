6a0ae67ae78ec3e3aeccf5ee3db03e16b73664dd
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-04-16 18:49:06 +0700

    feat: create all model and migrations

---
+++ database/migrations/2025_04_16_113216_create_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reported_by');
+            $table->unsignedBigInteger('reported_user');
+            $table->foreign('reported_by')->references('id')->on('users');
+            $table->foreign('reported_user')->references('id')->on('users');
+            $table->longText('reasons');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
