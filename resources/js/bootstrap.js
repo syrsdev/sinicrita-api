68be53c8bf828bec8e330c2cf8ed64f4f1c8b73e
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-06-26 18:40:57 +0700

    feat: install reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,3 +5,11 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
