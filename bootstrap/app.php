3720c42b3f3ac49591a245c15443d9d3a35be1f8
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-09 13:43:05 +0700

    fix: configuration reverb

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
         $middleware->append(StartSession::class);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
+    })->withBroadcasting(
+        __DIR__ . '/../routes/channels.php',
+        ['prefix' => 'api', 'middleware' => ['api', 'auth:sanctum']],
+    )->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
