b0aeb875c1dac636eecce58ebbbbb3d10e8277f8
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-09 13:43:37 +0700

    feat: show list chat

--- app/Http/Controllers/Chat/ChatController.php
+++ app/Http/Controllers/Chat/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Chat;
 
+use App\Events\SessionCreated;
 use App\Http\Controllers\Controller;
 use App\Models\chat_session;
 use Illuminate\Http\Request;
@@ -10,17 +11,31 @@ class ChatController extends Controller
 {
     public function createSession(Request $request)
     {
-        try { 
+        try {
             $credentials = $request->validate([
                 'user1_id' => 'required|exists:users,id',
                 'user2_id' => 'required|exists:users,id',
             ]);
 
-            if ($credentials['user1_id']   != $credentials['user2_id']) {
+            if ($credentials['user1_id'] != $credentials['user2_id']) {
                 $session = chat_session::create($credentials);
+                event(new SessionCreated($session));
             }
 
             return response()->json(['status' => 'success', 'statusCode' => '200', 'message' => 'Anda sudah bisa berinteraksi langsung', 'session_id' => $session->id]);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'statusCode' => '500', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function listChat($user_id)
+    {
+        try {
+            $data = chat_session::where('user1_id', $user_id)
+                ->orWhere('user2_id', $user_id)
+                ->get();
+
+            return response()->json(['status' => 'success', 'statusCode' => '200', 'data' => $data]);
         } catch (\Exception $e) {
             return response()->json(['status' => 'error', 'statusCode' => '500', 'message' => $e->getMessage()], 500);
         }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Chat\ChatController;
 use App\Http\Controllers\v1\Auth\AuthController;
 use App\Http\Controllers\v1\Post\PostController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/v1')->group(function () {
@@ -11,6 +12,9 @@ Route::prefix('/v1')->group(function () {
     Route::post('/login', AuthController::class . '@login');
 
     Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/broadcasting/auth', function (Request $request) {
+            return Broadcast::auth($request);
+        });
 
         Route::get('/user', function (Request $request) {
             return $request->user();
@@ -26,10 +30,7 @@ Route::prefix('/v1')->group(function () {
         });
         Route::prefix('/chat')->group(function () {
             Route::post('/session', [ChatController::class, 'createSession']);
-            // Route::post('/', [PostController::class, 'store']);
-            // Route::get('/detail/{slug}', [PostController::class, 'show']);
-            // Route::put('/detail/{slug}', [PostController::class, 'update']);
-            // Route::delete('/detail/{slug}/delete', [PostController::class, 'destroy']);
+            Route::get('/getlistchat/{user_id}', [ChatController::class, 'listChat']);
         });
     });
 });
