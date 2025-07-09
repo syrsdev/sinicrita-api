464ffda977c83fd9e5e134594cfb85958cbbf1e8
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-07-09 13:43:53 +0700

    feat: broadcast new chat session

--- routes/channels.php
+++ routes/channels.php
@@ -3,10 +3,6 @@
 use App\Models\chat_session;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('chat.{sessionId}', function ($user, $sessionId) {
-    $session = chat_session::find($sessionId);
-
-    return $session && (
-        $user->id === $session->user1_id || $user->id === $session->user2_id
-    );
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
 });
