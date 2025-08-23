8832de5f745a6efd6b7894a25303ed510511a193
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-08-23 18:48:51 +0700

    chore: call model

--- app/Models/calls.php
+++ app/Models/calls.php
@@ -4,7 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class calls extends Model
+class Call extends Model
 {
-    //
+    protected $fillable = [
+        'chat_id',
+        'status'
+    ];
+
+    public function chat()
+    {
+        return $this->belongsTo(chat_session::class, 'chat_id');
+    }
 }
