966c0316caed08241a7e38dbef4a6e4d5469273b
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-10-24 20:27:48 +0700

    chore: add change password controller

---
+++ app/Http/Controllers/v1/Auth/PasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PasswordController extends Controller
+{
+    public function ChangePassword(Request $request) {}
+}
