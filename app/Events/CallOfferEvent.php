876f8685641686f13879a14b32acfa213afac501
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-23 14:02:21 +0700

    fix: shouldBroadcast to shouldBroadcastNow

--- app/Events/CallOfferEvent.php
+++ app/Events/CallOfferEvent.php
@@ -6,12 +6,12 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class CallOfferEvent implements ShouldBroadcast
+class CallOfferEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
